0efc35367a6d94b96b02ff61539efda66d51b8b7
Aryan Patil <aryanitinpatil@gmail.com>
2024-02-10 00:06:43 +0530

    security config
    securityfilterchain impl, passencoder,authmanager

---
+++ E-Commerce/src/main/java/com/aryan/ecom/config/WebSecurityConfiguration.java
@@ -0,0 +1,61 @@
+package com.aryan.ecom.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+
+import com.aryan.ecom.filters.JwtRequestFilter;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfiguration {
+
+	private final JwtRequestFilter jwtRequestFilter;
+	
+
+	@Bean
+	MvcRequestMatcher.Builder mvc(HandlerMappingIntrospector introspector) {
+		return new MvcRequestMatcher.Builder(introspector);
+	}
+
+	@SuppressWarnings("deprecation")
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http, MvcRequestMatcher.Builder mvc) throws Exception {
+		return http.csrf(csrf -> csrf.disable())
+	            .authorizeRequests(auth -> auth
+	            		.requestMatchers(mvc.pattern("/authenticate"),mvc.pattern("/sign-up"),mvc.pattern("/order/**")).permitAll()
+	            		.requestMatchers(mvc.pattern(".api/**")).authenticated()
+	            )
+	            .sessionManagement(sessionManagement -> sessionManagement
+	                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+	            )
+	            .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
+	            .build();
+//		
+		
+	}
+
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception{
+		return config.getAuthenticationManager();
+	}
+}
