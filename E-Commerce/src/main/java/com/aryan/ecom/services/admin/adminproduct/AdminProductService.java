13afe4c8a50f634697a03f7ad61b1eee13258d2a
Aryan Patil <aryanitinpatil@gmail.com>
2024-02-10 00:19:19 +0530

    add product service

---
+++ E-Commerce/src/main/java/com/aryan/ecom/services/admin/adminproduct/AdminProductService.java
@@ -0,0 +1,11 @@
+package com.aryan.ecom.services.admin.adminproduct;
+
+import java.util.List;
+
+import com.aryan.ecom.dto.ProductDto;
+
+public interface AdminProductService {
+	
+	ProductDto addProduct(ProductDto productDto) throws Exception;
+	
+}
---
+++ E-Commerce/src/main/java/com/aryan/ecom/services/admin/adminproduct/AdminProductServiceImpl.java
@@ -0,0 +1,39 @@
+package com.aryan.ecom.services.admin.adminproduct;
+
+import java.lang.StackWalker.Option;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+
+import com.aryan.ecom.dto.ProductDto;
+import com.aryan.ecom.model.Category;
+import com.aryan.ecom.model.Product;
+import com.aryan.ecom.repository.CategoryRepository;
+import com.aryan.ecom.repository.ProductRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AdminProductServiceImpl implements AdminProductService {
+
+	private final ProductRepository productRepository;
+
+	private final CategoryRepository categoryRepository;
+
+	public ProductDto addProduct(ProductDto productDto) throws Exception {
+		Product product = new Product();
+		product.setName(productDto.getName());
+		product.setDescription(productDto.getDescription());
+		product.setPrice(productDto.getPrice());
+		product.setImg(productDto.getImg().getBytes());
+
+		Category category = categoryRepository.findById(productDto.getCategoryId()).orElseThrow();
+		product.setCategory(category);
+
+		return productRepository.save(product).getDto();
+	}
+
+}
