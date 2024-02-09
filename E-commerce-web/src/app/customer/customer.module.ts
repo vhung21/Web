6ac02368e043b6060b4f4facebe3f11808f87689
Aryan Patil <aryanitinpatil@gmail.com>
2024-02-09 23:51:44 +0530

    customer module imports
    forms and materialUI and httpclient

---
+++ E-commerce-web/src/app/customer/customer.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CustomerRoutingModule } from './customer-routing.module';
+import { CustomerComponent } from './customer.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DemoAngularMaterailModule } from '../DemoAngularMaterialModule';
+
+
+@NgModule({
+  declarations: [
+    CustomerComponent,
+    DashboardComponent
+  ],
+  imports: [
+    CommonModule,
+    CustomerRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    DemoAngularMaterailModule
+  ]
+})
+export class CustomerModule { }
