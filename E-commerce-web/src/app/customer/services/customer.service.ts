47153e3ad079fdb3d270ee991653e13aa8ca00d0
Aryan Patil <aryanitinpatil@gmail.com>
2024-02-09 23:52:08 +0530

    customer services on product and auth

---
+++ E-commerce-web/src/app/customer/services/customer.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { UserStorageService } from '../../services/storage/user-storage.service';
+
+const BASIC_URL = "http://localhost:8080/"
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomerService {
+
+  constructor(private http:HttpClient) { }
+
+  getAllProducts(): Observable<any> {
+    return this.http.get(BASIC_URL + 'api/customer/products', {
+      headers: this.createAuthorizationHeader()
+    });
+  }
+  
+  getAllProductsByName(name:any): Observable<any> {
+    return this.http.get(BASIC_URL + `api/customer/search/${name}`, {
+      headers: this.createAuthorizationHeader()
+    });
+  }
+
+  private createAuthorizationHeader(): HttpHeaders {
+    return new HttpHeaders().set(
+      'Authorization', 'Bearer' + UserStorageService.getToken()
+    )
+  }
+}
