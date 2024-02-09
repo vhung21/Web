e8e4c3d29e6d737fc35f494d9f807c7417bf305a
Aryan Patil <aryanitinpatil@gmail.com>
2024-02-09 23:47:58 +0530

    admin services on product and categories completed

---
+++ E-commerce-web/src/app/admin/service/admin.service.ts
@@ -0,0 +1,57 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { UserStorageService } from '../../services/storage/user-storage.service';
+
+const BASIC_URL = "http://localhost:8080/"
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminService {
+
+  constructor(private http: HttpClient) { }
+
+  addCategory(categoryDto: any): Observable<any> {
+    return this.http.post(BASIC_URL + 'api/admin/category', categoryDto, {
+      headers: this.createAuthorizationHeader()
+    });
+  }
+
+  getAllCategories(): Observable<any> {
+    return this.http.get(BASIC_URL + 'api/admin/categories', {
+      headers: this.createAuthorizationHeader()
+    });
+  }
+
+  addProduct(productDto: any): Observable<any> {
+    return this.http.post(BASIC_URL + 'api/admin/product', productDto, {
+      headers: this.createAuthorizationHeader()
+    });
+  }  
+
+  getAllProducts(): Observable<any> {
+    return this.http.get(BASIC_URL + 'api/admin/products', {
+      headers: this.createAuthorizationHeader()
+    });
+  }
+  
+  getAllProductsByName(name:any): Observable<any> {
+    return this.http.get(BASIC_URL + `api/admin/search/${name}`, {
+      headers: this.createAuthorizationHeader()
+    });
+  }
+
+  
+  deleteProduct(productId: any): Observable<any> {
+    return this.http.delete(BASIC_URL + `api/admin/product/${productId}`, {
+      headers: this.createAuthorizationHeader()
+    });
+  } 
+  private createAuthorizationHeader(): HttpHeaders {
+    return new HttpHeaders().set(
+      'Authorization', 'Bearer' + UserStorageService.getToken()
+    )
+  }
+  
+}
