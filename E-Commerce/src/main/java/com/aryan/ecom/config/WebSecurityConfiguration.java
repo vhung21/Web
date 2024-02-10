b0cda392747a93745d5b116d1f798c1df043206d
Aryan Patil <aryanitinpatil@gmail.com>
2024-02-10 15:06:22 +0530

    public swagger endpoints
    api-docs,swagger-resources,swagger-ui

--- E-Commerce/src/main/java/com/aryan/ecom/config/WebSecurityConfiguration.java
+++ E-Commerce/src/main/java/com/aryan/ecom/config/WebSecurityConfiguration.java
@@ -12,6 +12,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 import com.aryan.ecom.filters.JwtRequestFilter;
@@ -20,6 +21,7 @@ import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
+@EnableWebMvc
 @RequiredArgsConstructor
 public class WebSecurityConfiguration {
 
@@ -34,23 +36,23 @@ public class WebSecurityConfiguration {
 	@SuppressWarnings("deprecation")
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http, MvcRequestMatcher.Builder mvc) throws Exception {
-//		return http.csrf(csrf -> csrf.disable())
-//				.authorizeHttpRequests(
-//						requests -> requests.requestMatchers("/authenticate", "/sign-up", "/order/**").permitAll())
-//				.authorizeRequests(requests -> requests.requestMatchers("/api/**").authenticated())
-//				.sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-//				.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class).build();
+
 		return http.csrf(csrf -> csrf.disable())
 	            .authorizeRequests(auth -> auth
-	            		.requestMatchers(mvc.pattern("/authenticate"),mvc.pattern("/sign-up"),mvc.pattern("/order/**")).permitAll()
+	            		.requestMatchers(mvc.pattern("/authenticate"),
+	            				mvc.pattern("/sign-up"),
+	            				mvc.pattern("/order/**"),
+	            				mvc.pattern("/v3/api-docs"),
+	            				mvc.pattern("/swagger-resources/**"),
+	            				mvc.pattern("/swagger-ui/**"),
+	            				mvc.pattern("/webjars/**")).permitAll()
 	            		.requestMatchers(mvc.pattern(".api/**")).authenticated()
 	            )
 	            .sessionManagement(sessionManagement -> sessionManagement
 	                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 	            )
 	            .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
-	            .build();
-//		
+	            .build();	
 		
 	}
 
