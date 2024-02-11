5cf93e64dbf1fc1d57347c186057e43ddc4867c0
Aryan Patil <aryanitinpatil@gmail.com>
2024-02-12 01:36:06 +0530

    mapping cartItems to DTO

--- E-Commerce/src/main/java/com/aryan/ecom/model/CartItems.java
+++ E-Commerce/src/main/java/com/aryan/ecom/model/CartItems.java
@@ -3,6 +3,8 @@ package com.aryan.ecom.model;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
+import com.aryan.ecom.dto.CartItemsDto;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -37,5 +39,16 @@ public class CartItems {
 	@OneToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "order_id")
 	private Order order;
-
+ 
+	public CartItemsDto getCartDto() {
+		CartItemsDto cartItemsDto = new CartItemsDto();
+		cartItemsDto.setId(id);
+		cartItemsDto.setPrice(price);
+		cartItemsDto.setProductId(product.getId());
+		cartItemsDto.setQuantity(quantity);
+		cartItemsDto.setUserId(user.getId());
+		cartItemsDto.setProductName(product.getName());
+		cartItemsDto.setReturnedImage(product.getImg());
+		return cartItemsDto;
+	}
 }
