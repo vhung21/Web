5b84917e20af590a22935e29ab3ffd4f1217cc4f
Aryan Patil <aryanitinpatil@gmail.com>
2024-02-12 01:37:12 +0530

    Create CartItemsDto.java

---
+++ E-Commerce/src/main/java/com/aryan/ecom/dto/CartItemsDto.java
@@ -0,0 +1,15 @@
+package com.aryan.ecom.dto;
+
+import lombok.Data;
+
+@Data
+public class CartItemsDto {
+	private Long id;
+	private Long price;
+	private Long quantity;
+	private Long productId;
+	private Long orderId;
+	private String productName;
+	private byte[] returnedImage;
+	private Long userId;
+}
