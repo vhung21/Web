0a82df9fc7d13e908c2aebc94ddb736d5035494e
Aryan Patil <aryanitinpatil@gmail.com>
2024-02-12 01:36:33 +0530

    Coupon existsByCode and findByCode

---
+++ E-Commerce/src/main/java/com/aryan/ecom/repository/CouponRepository.java
@@ -0,0 +1,14 @@
+package com.aryan.ecom.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.aryan.ecom.model.Coupon;
+
+@Repository
+public interface CouponRepository extends JpaRepository<Coupon, Long>{
+	boolean existsByCode(String code);
+	Optional<Coupon> findByCode(String code);
+}
