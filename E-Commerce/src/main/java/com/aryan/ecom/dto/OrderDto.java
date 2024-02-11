7dbcbd9e39d458ed3ec1e99a21dbff9de91d8b61
Aryan Patil <aryanitinpatil@gmail.com>
2024-02-12 01:36:57 +0530

    Create OrderDto.java

---
+++ E-Commerce/src/main/java/com/aryan/ecom/dto/OrderDto.java
@@ -0,0 +1,40 @@
+package com.aryan.ecom.dto;
+
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+import com.aryan.ecom.enums.OrderStatus;
+import com.aryan.ecom.model.CartItems;
+import com.aryan.ecom.model.User;
+
+import lombok.Data;
+
+@Data
+public class OrderDto {
+	private Long id;
+
+	private String orderDescription;
+
+	private Date date;
+
+	private Long amount;
+
+	private String address;
+
+	private String payment;
+
+	private OrderStatus orderStatus;
+
+	private Long totalAmount;
+
+	private Long discount;
+
+	private UUID trackingId;
+
+	private String userName;
+
+	private List<CartItemsDto> cartItems;
+	
+	private String couponName;
+}
