7b4b1610a7e34d84c779ad29756f7946e3e1186f
Aryan Patil <aryanitinpatil@gmail.com>
2024-02-14 01:08:22 +0530

    Create PlaceOrderDto.java

---
+++ E-Commerce/src/main/java/com/aryan/ecom/dto/PlaceOrderDto.java
@@ -0,0 +1,14 @@
+package com.aryan.ecom.dto;
+
+import lombok.Data;
+
+@Data
+public class PlaceOrderDto {
+	
+	private Long userId;
+	
+	private String address;
+	
+	private String orderDescription;
+	
+}
