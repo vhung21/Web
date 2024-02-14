c9276c141531f5b216f2f5a85179cedbeaf2a240
Aryan Patil <aryanitinpatil@gmail.com>
2024-02-14 18:44:07 +0530

    /myOrders/{userId} customer endpoint
    boot

--- E-Commerce/src/main/java/com/aryan/ecom/controller/customer/CartController.java
+++ E-Commerce/src/main/java/com/aryan/ecom/controller/customer/CartController.java
@@ -1,5 +1,7 @@
 package com.aryan.ecom.controller.customer;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 
 import org.springframework.http.ResponseEntity;
@@ -59,6 +61,11 @@ public class CartController {
 	@PostMapping("/placeOrder")
 	public ResponseEntity<OrderDto> placeOrder(@RequestBody PlaceOrderDto placeOrderDto) {
 		return ResponseEntity.status(HttpStatus.CREATED).body(cartService.placeOrder(placeOrderDto));
+	}
+	
+	@GetMapping("/myOrders/{userId}")
+	public ResponseEntity<List<OrderDto>> getMyPlacedOrders(@PathVariable Long userId){
+		return ResponseEntity.ok(cartService.getMyPlacedOrders(userId));
 	}
 
 }
