1f63f7c99107e13d625372d7c485ae6387e883c3
Aryan Patil <aryanitinpatil@gmail.com>
2024-02-14 18:43:23 +0530

    order/{orderId}/{status} endpoint

--- E-Commerce/src/main/java/com/aryan/ecom/controller/admin/AdminOrderController.java
+++ E-Commerce/src/main/java/com/aryan/ecom/controller/admin/AdminOrderController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,10 +19,19 @@ import lombok.RequiredArgsConstructor;
 public class AdminOrderController {
 
 	private final AdminOrderService adminOrderService;
-	
+
 	@GetMapping("/placedOrders")
-	public ResponseEntity<List<OrderDto>> getAllPlacedOrders(){
+	public ResponseEntity<List<OrderDto>> getAllPlacedOrders() {
 		return ResponseEntity.ok(adminOrderService.getAllPlacedOrders());
 	}
-	
+
+	@GetMapping("/order/{orderId}/{status}")
+	public ResponseEntity<?> changeOrderStatus(@PathVariable Long orderId, @PathVariable String status) {
+		OrderDto orderDto = adminOrderService.changeOrderStatus(orderId, status);
+		if (orderDto == null)
+			return ResponseEntity.badRequest().body("Something Went Wrong!!");
+
+		return ResponseEntity.ok(orderDto);
+	}
+
 }
