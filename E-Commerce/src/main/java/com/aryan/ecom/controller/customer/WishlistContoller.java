6c02b9af6eebf0ec29e3bc85dd82a0009747af51
Aryan Patil <aryanitinpatil@gmail.com>
2024-02-18 18:51:57 +0530

    /wishlist customer endpoint

---
+++ E-Commerce/src/main/java/com/aryan/ecom/controller/customer/WishlistContoller.java
@@ -0,0 +1,32 @@
+package com.aryan.ecom.controller.customer;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.aryan.ecom.dto.WishlistDto;
+ import com.aryan.ecom.services.customer.wishlist.WishlistService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/customer")
+public class WishlistContoller {
+	private final WishlistService wishlistService;
+	
+	@PostMapping("/wishlist")
+	public ResponseEntity<?> addProductToWishlist(@RequestBody WishlistDto wishlistDto){
+		WishlistDto posteWishlistDto = wishlistService.addProductToWishlist(wishlistDto);
+		if(posteWishlistDto==null) return ResponseEntity.status(HttpStatus.BAD_GATEWAY).body("somewhing went wrong");
+		return ResponseEntity.status(HttpStatus.CREATED).body(posteWishlistDto);
+	}
+	
+}
