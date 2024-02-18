6434f71417a715bf5cd470fac48785e1e55ff8f9
Aryan Patil <aryanitinpatil@gmail.com>
2024-02-18 18:24:57 +0530

    postFAQ endpoint

--- E-Commerce/src/main/java/com/aryan/ecom/controller/admin/AdminProductController.java
+++ E-Commerce/src/main/java/com/aryan/ecom/controller/admin/AdminProductController.java
@@ -2,6 +2,7 @@ package com.aryan.ecom.controller.admin;
 
 import java.util.List;
 
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -9,12 +10,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.service.annotation.PutExchange;
 
+import com.aryan.ecom.dto.FAQDto;
 import com.aryan.ecom.dto.ProductDto;
 import com.aryan.ecom.services.admin.adminproduct.AdminProductService;
+import com.aryan.ecom.services.admin.faq.FAQService;
 
+import io.jsonwebtoken.io.IOException;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -24,11 +31,12 @@ public class AdminProductController {
 
 	private final AdminProductService adminProductService;
 
+	private final FAQService faqService;
+
 	@PostMapping("/product")
 	public ResponseEntity<ProductDto> addProduct(@ModelAttribute ProductDto productDto) throws Exception {
 		ProductDto productDto1 = adminProductService.addProduct(productDto);
 		return ResponseEntity.status(HttpStatus.CREATED).body(productDto1);
-
 	}
 
 	@GetMapping("/products")
@@ -49,4 +57,8 @@ public class AdminProductController {
 		return deleted ? ResponseEntity.noContent().build() : ResponseEntity.notFound().build();
 	}
 
+	@PostMapping("/faq/{productId}")
+	public ResponseEntity<FAQDto> postFAQ(@PathVariable Long productId, @RequestBody FAQDto faqDto) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(faqService.postFAQ(productId, faqDto));
+	}
 }
