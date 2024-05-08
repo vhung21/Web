d3ce1e89424dadc68e81a8038dfe4122ce43b6dd
Aryan Patil <aryanitinpatil@gmail.com>
2024-05-08 23:29:43 +0530

    CategoryRepositoryTest

---
+++ E-Commerce/src/test/java/com/aryan/ecom/repository/CategoryRepositoryTest.java
@@ -0,0 +1,17 @@
+package com.aryan.ecom.repository;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoryRepositoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
