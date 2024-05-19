4424bc624e85df1aad5908041c28d655fd3da722
Aryan Patil <aryanitinpatil@gmail.com>
2024-05-19 22:01:15 +0530

    CategoryServiceImplTest

--- E-Commerce/src/test/java/com/aryan/ecom/repository/CategoryRepositoryTest.java
+++ E-Commerce/src/test/java/com/aryan/ecom/repository/CategoryRepositoryTest.java
@@ -2,13 +2,16 @@ package com.aryan.ecom.repository;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CategoryRepositoryTest {
-//    TODO : remaining
+    // No custom method
+
     @BeforeEach
     void setUp() {
+
     }
 
     @AfterEach
