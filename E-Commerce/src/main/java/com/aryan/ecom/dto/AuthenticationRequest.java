360a77cf97c5665fa8887823b49484272b158b7b
Aryan Patil <aryanitinpatil@gmail.com>
2024-05-23 13:20:07 +0530

    AuthControllerTest unit tests

--- E-Commerce/src/main/java/com/aryan/ecom/dto/AuthenticationRequest.java
+++ E-Commerce/src/main/java/com/aryan/ecom/dto/AuthenticationRequest.java
@@ -1,8 +1,14 @@
 package com.aryan.ecom.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor
 public class AuthenticationRequest {
 	private String username;
 	private String password;
