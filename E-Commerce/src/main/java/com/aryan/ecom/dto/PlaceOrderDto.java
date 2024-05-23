9b85032461a24da1c8eb45ffcce57244e75c1a03
Aryan Patil <aryanitinpatil@gmail.com>
2024-05-23 21:16:02 +0530

    CartServiceImplTest unit test

--- E-Commerce/src/main/java/com/aryan/ecom/dto/PlaceOrderDto.java
+++ E-Commerce/src/main/java/com/aryan/ecom/dto/PlaceOrderDto.java
@@ -1,8 +1,14 @@
 package com.aryan.ecom.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor
 public class PlaceOrderDto {
 	
 	private Long userId;
